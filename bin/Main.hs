498c60e26db9d8470253cd048f6ea1b0dddc2421
John Van Enk <vanenkj@gmail.com>
2015-04-26 00:58:22 -0700

    Fold remaining Ctx functionality up into Main.hs. Remove Ctx.

--- bin/Main.hs
+++ bin/Main.hs
@@ -2,11 +2,12 @@ module Main where
 
 import qualified Cauterize.Specification as Sp
 
-import Cauterize.C11Ref.Ctx
 import Cauterize.C11Ref.FileSystem
 import Cauterize.C11Ref.Generate
 import Cauterize.C11Ref.Options
 import Cauterize.C11Ref.Static
+
+import Data.Text.Lazy (unpack)
 
 main :: IO ()
 main = runWithOptions caut2c11
@@ -14,12 +15,13 @@ main = runWithOptions caut2c11
 caut2c11 :: Caut2C11Opts -> IO ()
 caut2c11 (Caut2C11Opts { specFile = sf, outputDirectory = od }) = createGuard od $ do
   spec <- loadSpec
-  let ctx = ctxFromSpec spec
+  let baseName = unpack $ Sp.specName spec
 
   copyStaticFilesTo od
-  generateDynamicFiles od ctx spec
+  generateDynamicFiles od baseName spec
 
   where
+
     loadSpec :: IO Sp.Spec
     loadSpec = do
       s <- Sp.parseFile sf
--- lib/Cauterize/C11Ref/Generate.hs
+++ lib/Cauterize/C11Ref/Generate.hs
@@ -1,13 +1,12 @@
 module Cauterize.C11Ref.Generate where
 
-import Cauterize.C11Ref.Ctx
 import Cauterize.C11Ref.LibHFile
 import Cauterize.C11Ref.LibCFile
 import System.FilePath.Posix
 import qualified Cauterize.Specification as Sp
 
-generateDynamicFiles :: FilePath -> Ctx -> Sp.Spec -> IO ()
-generateDynamicFiles path ctx spec = do
-  writeFile (path `combine` hFile ctx) (hFileFromSpec spec)
-  writeFile (path `combine` cFile ctx) (cFileFromSpec spec)
+generateDynamicFiles :: FilePath -> String -> Sp.Spec -> IO ()
+generateDynamicFiles path baseName spec = do
+  writeFile (path `combine` (baseName ++ ".h")) (hFileFromSpec spec)
+  writeFile (path `combine` (baseName ++ ".c")) (cFileFromSpec spec)
 
