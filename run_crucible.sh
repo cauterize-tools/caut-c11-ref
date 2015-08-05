c81fd51ab997f91e7cb630bde875b94dedcba674
John Van Enk <vanenkj@gmail.com>
2015-08-05 12:04:11 -0600

    Retool run_crucible to use stack. Add the crucible dependency to stack.yaml

--- run_crucible.sh
+++ run_crucible.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-./.cabal-sandbox/bin/crucible tester \
-  --build-cmd="../../dist/build/caut-c11-ref/caut-c11-ref  --spec=%s --output=c11" \
+stack exec crucible -- tester \
+  --build-cmd="stack exec caut-c11-ref -- --spec=%s --output=c11" \
   --build-cmd="cd c11 && make" \
   --run-cmd="./c11/test_client" \
   --schema-count=1 \
