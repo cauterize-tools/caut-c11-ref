f38831146e845890b8c1bec51794b2f5c3077bb2
Jay Kickliter <jay@kickliter.com>
2015-11-23 09:35:41 -0700

    Make record and union comparisons return the correct value

--- lib/Cauterize/C11Ref/LibCFile/Comparators.hs
+++ lib/Cauterize/C11Ref/LibCFile/Comparators.hs
@@ -73,7 +73,7 @@ compareRecord fs = chompNewline [i|
 
 #{fieldComps}
 
-    return caut_ord_eq;
+    return _c_o;
 |]
   where
     fieldComps = intercalate "\n" $ map compareRecordField fs
@@ -101,7 +101,7 @@ compareUnion n fs = chompNewline [i|
 #{fieldComps}
     }
 
-    return caut_ord_eq;|]
+    return _c_o;|]
   where
     fieldComps = intercalate "\n" $ map (compareUnionField n) fs
 
